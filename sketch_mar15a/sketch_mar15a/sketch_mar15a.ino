5900972b1f612444e0e6b01cd3a78870e57982b4
serdarkir <serdarkir@anadolu.edu.tr>
2015-03-15 22:24:16 +0200

    Update sketch_mar15a.ino

--- sketch_mar15a/sketch_mar15a/sketch_mar15a.ino
+++ sketch_mar15a/sketch_mar15a/sketch_mar15a.ino
@@ -1,26 +1,30 @@
-const int ledPinleri[] = {9,10, 11, 12, 13}; //ledin takılı olduğu pinler olan 10,11,12,13,14 tanımlandı.
+const int ledPinleri[] = {9,10, 11, 12, 13}; //ledlerin takılı olduğu pinler olan 9,10,11,12,13 tanımlandı.
   
 void setup() {
+  
   for (int i = 0; i < 5; i++)
       pinMode(ledPinleri[i], OUTPUT); // Bütün pinler sırayla çıkış olarak tanımlandı.
 }
+
 void loop() {
+  
   for (int i = 0; i < 4; i++) {
-    digitalWrite(ledPinleri[i], HIGH); 10.pindeki led yandı.
+    digitalWrite(ledPinleri[i], HIGH); //9.pindeki led yandı.
     delay(50); //50 ms bekleme süresi
-    digitalWrite(ledPinleri[i+1], HIGH); 11.pindeki led yandı.
+    digitalWrite(ledPinleri[i+1], HIGH);// 10.pindeki led yandı.
     delay(50); //50 ms bekleme süresi
-    digitalWrite(ledPinleri[i], LOW);// 10.pindeki led söndü
+    digitalWrite(ledPinleri[i], LOW);// 9.pindeki led söndü
     delay(100); //100 ms bekleme süresi
   }
-//14.pine kadar bu işlem devam eder.
+//13.pine kadar bu işlem devam eder.
  
   for (int i = 5; i > 0; i--) {
-    digitalWrite(ledPinleri[i], HIGH); 14.pindeki led yandı.
+    digitalWrite(ledPinleri[i], HIGH); //13.pindeki led yandı.
     delay(50);//50 ms bekleme süresi
-    digitalWrite(ledPinleri[i-1], HIGH); 13.pindeki led yandı.
+    digitalWrite(ledPinleri[i-1], HIGH); //12.pindeki led yandı.
     delay(50);//50 ms bekleme süresi
-    digitalWrite(ledPinleri[i], LOW);14.pindeki led söndü.
+    digitalWrite(ledPinleri[i], LOW);//13.pindeki led söndü.
     delay(100);//100 ms bekleme süresi
   }
-//10.pine kadar bu işlem devam eder.</code></pre>
+//9.pine kadar bu işlem devam eder.
+}
